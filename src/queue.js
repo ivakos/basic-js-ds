b09a1d309c4626cdb0ca24bbf938fdf8dd636737
Ivan <ivan.kasenka@gmail.com>
2022-05-03 02:20:26 +0300

    feat: part2

--- src/queue.js
+++ src/queue.js
@@ -14,15 +14,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
 class Queue {
-
-  getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this.top =null;
+    this.length = 0;
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  getUnderlyingList() {
+    if(this.length) 
+      return this.top;
+    else 
+      return null;
+  }
+
+  enqueue(value) {
+
   }
 
   dequeue() {
